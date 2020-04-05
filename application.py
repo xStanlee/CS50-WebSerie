f8eb4d120b98a43de0b998c817240b06b1e9369b
Patryk <patryk_stachura@yahoo.com>
2020-04-05 20:05:09 +0200

    Add models
    Add models.py files where's stored model of DB tables Connect work time to code !

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 import os
 import requests
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from models import *
 
 app = Flask(__name__)
 
@@ -15,42 +17,37 @@ if not os.getenv("DATABASE_URL"):
 # Configure sqlalchemy to connect my DB
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#db.init_app(app)
+db.init_app(app)
 
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
 
 # Request for jsonFile
 res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "cIAWnULXTSoqvTIKqOMdTQ", "isbns": "9781632168146"})
 resJson = res.json()
 
-class Book:
-    def __init__(self, title, author, year, isbn, review_count, avarge_score):
-        self.title = title
-        self.author = author
-        self.year = year
-        self.isbn = isbn
-        self.review_count = review_count
-        self.avarge_score = avarge_score
-class User:
-    def __init__(self, first_name, last_name, username, password, email, phone):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.password = password
-        self.email = email
-        self.phone = phone
-
 @app.route("/")
+def empty():
+    usersName = Users.query.first()
+    return(f"WORK ? HASSWORD - {usersName.password} LOGINS - {usersName.username} PHONE {usersName.phone}")#redirect(url_for('login'))
+
+@app.route("/login", methods=['POST', 'GET'])
 def login():
-    awatar = Book('Awatar', 'JakisDzonson', 1994, 23212321, 42, 4.11)
-    return render_template('login.html', title="Login page")
+    if request.method == "POST":
+        user = request.form["username"]
+        password = request.form["pass"]
+    else:
+        return render_template('login.html', title="Login page")
+
+@app.route("/test/<usr>/<psw>")
+def test(usr, psw):
+    return f"USER : {usr}, PASSWORD: {psw}"
 
 @app.route("/registration/")
 def registration():
---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Book:
+    def __init__(self, title, author, year, isbn, review_count, avarge_score):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.isbn = isbn
+        self.review_count = review_count
+        self.avarge_score = avarge_score
+
+class Users(db.Model):
+    #def __init__(self, username, password, first_name, last_name, phone, email):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String, nullable=False)
